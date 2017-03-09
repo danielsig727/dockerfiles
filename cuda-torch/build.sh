ab021b6986d42dbdcca0afe8cc07211f6a46d45c
Daniel <danielsig727@gmail.com>
2017-03-09 15:49:39 +0800

    pushing to docker hub

--- cuda-torch/build.sh
+++ cuda-torch/build.sh
@@ -5,4 +5,7 @@ TAG_PREFIX=
 DATE=$(date +"%y%m%d-%H%M%S")
 
 docker build -t ${REPO}:${TAG_PREFIX}latest \
-             -t ${REPO}:${TAG_PREFIX}${DATE} .
+             -t ${REPO}:${TAG_PREFIX}${DATE} . && \
+docker login && \
+docker push -t ${REPO}:${TAG_PREFIX}latest \
+            -t ${REPO}:${TAG_PREFIX}${DATE} .
