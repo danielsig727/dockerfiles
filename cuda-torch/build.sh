1db1fa96307d6923345f5b2211e8f4322d089a78
Daniel <danielsig727@gmail.com>
2017-03-09 16:46:44 +0800

    fix repo name

--- cuda-torch/build.sh
+++ cuda-torch/build.sh
@@ -1,11 +1,10 @@
 #!/bin/bash -x
 
-REPO=cuda-torch
+REPO=danielsig727/cuda-torch
 TAG_PREFIX=
 DATE=$(date +"%y%m%d-%H%M%S")
 
 nvidia-docker build --no-cache=true -t ${REPO}:${TAG_PREFIX}latest \
              -t ${REPO}:${TAG_PREFIX}${DATE} . && \
-docker login && \
 docker push -t ${REPO}:${TAG_PREFIX}latest \
             -t ${REPO}:${TAG_PREFIX}${DATE} .
