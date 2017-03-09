cf9f96ab91152bae4366c75c2f4ef9d93f98b41e
Daniel <danielsig727@gmail.com>
2017-03-09 16:07:02 +0800

    nvidia-docker

--- cuda-torch/build.sh
+++ cuda-torch/build.sh
@@ -4,7 +4,7 @@ REPO=cuda-torch
 TAG_PREFIX=
 DATE=$(date +"%y%m%d-%H%M%S")
 
-docker build -t ${REPO}:${TAG_PREFIX}latest \
+nvidia-docker build --no-cache=true -t ${REPO}:${TAG_PREFIX}latest \
              -t ${REPO}:${TAG_PREFIX}${DATE} . && \
 docker login && \
 docker push -t ${REPO}:${TAG_PREFIX}latest \
